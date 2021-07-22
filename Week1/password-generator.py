3022a3a3938b1bf4be9cb3c33d593d9382b17774
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-07-22 14:24:57 +0300

    commit

--- Week1/password-generator.py
+++ Week1/password-generator.py
@@ -1,1 +1,14 @@
-#! /usr/bin/python3
+def factorial(x):
+    if x==0:
+        return 1
+    else:
+        return factorial(x-1) * x
+
+print ("This is a simple program to find factorial of a number")
+try:
+    num=int(input("Please enter a number to calculate factorial\n(num)> "))
+except:
+    print("Enter an integer")
+
+mystring = "{} factorial is {}"
+print(mystring.format(num, factorial(num)))
