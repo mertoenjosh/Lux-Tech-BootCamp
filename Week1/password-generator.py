bb26c79a5b58c758a03dce6a750d415caac716b9
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-07-27 20:47:09 +0300

    commit

--- Week1/password-generator.py
+++ Week1/password-generator.py
@@ -1,31 +1,25 @@
 #! /usr/bin/python3
 import string, random
+
 # A helper function to split a string
 def split(all):
     return [char for char in all]
 
 all_strings = split(string.printable)
+
 # create lists to store each type of string
 digits = all_strings[0:10]
 lowerCase = all_strings[10:36]
 upperCase = all_strings[36:62]
 syms = all_strings[62:94]
 
-def lowerLetters(lower):
+def twoRandoms(coll):
     # return a list of 2 random lowerCase letters
-    return random.choices(lower, k=2)
-def upperLetters(upper):
-    # return a list of 2 random upperCase letters
-    return random.choices(upper, k=2)
-def numbers(vals):
-    # return a list 2 digits of type string
-    return random.choices(vals, k=2)
-def symbols(syms):
-    # return a list of 2 random symbols 'space' excluded
-    return random.choices(syms, k=2)
+    return random.choices(coll, k=2)
+
 
 # concatenate the returned strings in to one string
-temp = lowerLetters(lowerCase) + upperLetters(upperCase) + numbers(digits) + symbols(syms)
+temp = twoRandoms(lowerCase) + twoRandoms(upperCase) + twoRandoms(digits) + twoRandoms(syms)
 random.shuffle(temp)  # shuffle the final string
 
 # join the random list elements into a string
