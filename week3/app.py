e96ff503c8060ceac9af350418cfca0b19105982
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-08-11 14:53:36 +0300

    commit

--- week3/app.py
+++ week3/app.py
@@ -1,11 +1,36 @@
-from flask import Flask
-import sqlalchemy as db
-from sqlalchemy import create_engine
-from sqlalchemy import *
-import pymysql
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+# set configs
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'wiz'
+app.config['MYSQL_PASSWORD'] = 'kameoDB'
+app.config['MYSQL_DB'] = 'flask_test'
+
+mysql = MySQL(app)
+
+
+# set database cursor
+
+# set the routes
 @app.route('/')
-def hello_world():
-    return 'Hello, world!'
+def form():
+    return render_template('form.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method != "POST":
+        return "Use the sign up form"
+    
+    if request.method == 'POST':
+        details = request.form
+        name = details['name']
+        email = details['email']
+        message = details['message']
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO users(name, email, message) VALUES (%s, %s, %s)",(name, email, message))
+        cursor.connection.commit()
+        cursor.close()
+        return f'Created user {name}'
